8840605b6aeed0101b1f051082b1702ee0bf9353
Jawa350T12 <91010789+Jawa350T12@users.noreply.github.com>
2022-03-21 21:31:14 +0300

    MOVE

--- Tettris/Program.cs
+++ Tettris/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Threading;
 
 namespace Tettris
 {
     public class Program
     {
+        static int xx=0;
+        static String ss="";
+        static int yy = 3;
+
         static void GamePole()// Отрисовка игрового поля
         {
             Pole.GameWindow();
@@ -19,6 +24,9 @@ namespace Tettris
 
         static void RandomSpawn()//Случайная генерация фигур
         {
+            xx = 0;
+            ss = "";
+
             var rand = new Random();
 
             String[] s = { "Cub", "Stick", "NedoT", "GR", "GL", "ZR", "ZL" };
@@ -26,27 +34,91 @@ namespace Tettris
             switch (s[rand.Next(0, 7)])
             {
                 case "Cub":
-                    Figure.Cub(rand.Next(3,50), 3);
-                    break;
-                case "Stick":
-                    Figure.Stick(rand.Next(3, 50), 3);
-                    break;
-                case "NedoT":
-                    Figure.NedoT(rand.Next(3, 50), 3);
-                    break;
-                case "GR":
-                    Figure.GR(rand.Next(3, 50), 3);
-                    break;
-                case "GL":
-                    Figure.GL(rand.Next(3, 50), 3);
-                    break;
-                case "ZR":
-                    Figure.ZR(rand.Next(3, 50), 3);
-                    break;
-                case "ZL":
-                    Figure.ZL(rand.Next(3, 50), 3);
+                    xx = rand.Next(3, 50);
+                    ss = "Cub";
+                    Figure.Cub(xx, 3);
                     break;
 
+                case "Stick":
+                    xx = rand.Next(3, 50);
+                    ss = "Stick";
+                    Figure.Stick(xx, 3);
+                    break;
+
+                case "NedoT":
+                    xx = rand.Next(3, 50);
+                    ss = "NedoT";
+                    Figure.NedoT(xx, 3);
+                    break;
+
+                case "GR":
+                    xx = rand.Next(3, 50);
+                    ss = "GR";
+                    Figure.GR(xx, 3);
+                    break;
+
+                case "GL":
+                    xx = rand.Next(3, 50);
+                    ss = "GL";
+                    Figure.GL(xx, 3);
+                    break;
+
+                case "ZR":
+                    xx = rand.Next(3, 50);
+                    ss = "ZR";
+                    Figure.ZR(xx, 3);
+                    break;
+
+                case "ZL":
+                    xx = rand.Next(3, 50);
+                    ss = "ZL";
+                    Figure.ZL(xx, 3);
+                    break;
+
+            }
+        }
+
+        static void MoveFig(int x,String namefig)//Движение фигуры
+        {
+            
+            Console.Clear();
+            GamePole();
+            switch (namefig)
+            {
+                case "Cub":
+                    yy++;
+                    Figure.Cub(xx, yy);
+                    break;
+
+                case "Stick":
+                    yy++;
+                    Figure.Stick(xx, yy);
+                    break;
+
+                case "NedoT":
+                    yy++;
+                    Figure.NedoT(xx, yy);
+                    break;
+
+                case "GR":
+                    yy++;
+                    Figure.GR(xx, yy);
+                    break;
+
+                case "GL":
+                    yy++;
+                    Figure.GL(xx, yy);
+                    break;
+
+                case "ZR":
+                    yy++;
+                    Figure.ZR(xx, yy);
+                    break;
+
+                case "ZL":
+                    yy++;
+                    Figure.Cub(xx, yy);
+                    break;
             }
         }
 
@@ -54,6 +126,13 @@ namespace Tettris
         {
             GamePole();
             RandomSpawn();
+            int c = 0;
+            while (c < 47)
+            {
+                c++;
+                MoveFig(xx, ss);
+                Thread.Sleep(500);
+            }
         }
     }
 }
